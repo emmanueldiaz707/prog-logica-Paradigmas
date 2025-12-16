ace1a69cc0055d2ac1931cc74f764886c642d77b
Emmanuel Diaz <emmanueldiaz707@gmail.com>
2025-12-15 21:38:44 -0300

    agregar consignas parcial 2025

---
+++ parciales_viejos/parcial_2025.pl
@@ -0,0 +1,35 @@
+/*
+  Tomar una lista L sin sublistas y una cantidad C.
+  Devolver otra lista con las sumatorias de los C elementos de L
+  partiendo de cada posición, desde 1 hasta longitud(L)-C+1.
+  
+  L = [1,3,2,4,5,4,3,2,3,4,9,10,2,33]
+  C = 4
+
+  R = [10,14,15,16,14,12,12,18,26,25,54]
+*/
+
+
+
+
+
+
+
+
+
+
+
+/*
+  Tomar como entrada una lista L sin sublistas y una lista M con sublistas.
+  Devolver una lista con sublistas de la forma [E,N1,N2,...,Nr],
+  donde E es cada elemento de L y N1..Nr son los niveles de M en los cuales
+  se encuentran cada una de las ocurrencias de E.
+  
+  L no posee elementos repetidos.
+  Cada elemento E de L debe aparecer una sola vez en la lista resultante.
+  
+  L = [6,3,2,4,8,9]
+  M = [2,5,[4,7,3],5,[3,[4,9,2,10],6,5,7,4],9,2]
+
+  R = [[6,2],[3,2,2],[2,1,3,1],[4,2,3,2],[8],[9,3,1]]
+*/
