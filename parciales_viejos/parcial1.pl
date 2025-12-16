631f9d8949e697cf3ffdc178898edb688a726dd9
Emmanuel Diaz <emmanueldiaz707@gmail.com>
2025-12-16 19:47:57 -0300

    consignas parcial 1

---
+++ parciales_viejos/parcial1.pl
@@ -0,0 +1,24 @@
+/*
+   Tomar como entrada una lista sin sublistas L y un número N,
+   devolver una lista con los elementos de L que están en las posiciones múltiplos de N.
+
+   Por ejemplo, si N=3, las posiciones serían 3, 6, 9, etc.
+
+   L = [4, 5, 9, 6, 12, 8, 6, 5, 4, 3, 2, 3, 5, 10]
+   N = 3
+   
+   Resultado = [9, 8, 4, 3]
+*/
+
+
+
+/*
+   Dada una lista con sublistas L, devolver una lista donde el iésimo elemento
+   representa la sumatoria de los números contenidos en nivel i de profundidad de L.
+   
+   Si no hay elementos numéricos en un nivel específico,
+   ese nivel tendrá valor 0 en la lista resultante.
+   
+   L = [1, [2, 3], [[4, 5], 6], [7, [8, [9]]]]
+   R = Resultado = [1, 18, 17, 9]
+*/
