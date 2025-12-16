ff29db022bb38a3ab5a7de5d3cb5fc44a46ea728
Emmanuel Diaz <emmanueldiaz707@gmail.com>
2025-12-16 17:53:20 -0300

    consigna 1 parcial 2025

--- parciales_viejos/parcial_2025.pl
+++ parciales_viejos/parcial_2025.pl
@@ -7,17 +7,29 @@
   C = 4
 
   R = [10,14,15,16,14,12,12,18,26,25,54]
+  
+  
+  longitud(L) = 14
+  longitud(R) = longitud(L)-C+1 = 14-4+1 = 11
 */
 
-
-
-
-
-
-
-
-
-
+  n_elementos(_, 0, []).
+  n_elementos([X|XS], N, [X|YS]) :- N>0, N1 is N-1, n_elementos(XS,N1,YS).
+  
+  sumatoria([],0).
+  sumatoria([X|XS],S) :- sumatoria(XS,S1), S is S1+X.
+  
+  longitud([],0).
+  longitud([_|XS],S) :- longitud(XS,S1), S is S1+1.
+  
+  lista_sumatorias(L,C,Resultado) :- longitud(L,Long), Limite is Long-C+1, lista_sumatorias(L,C,Resultado,Limite).
+  lista_sumatorias(_, _, [], 0).
+  lista_sumatorias([X|XS], C, [Y|YS], Limite) :-
+     n_elementos([X|XS], C, Z),
+     sumatoria(Z, S),
+     Y is S,
+     Limite1 is Limite-1,
+     lista_sumatorias(XS, C, YS, Limite1).
 
 /*
   Tomar como entrada una lista L sin sublistas y una lista M con sublistas.
