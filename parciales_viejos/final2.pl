eb3bcd4e0d196b63fb0557cdf77cf1098bf418a7
Emmanuel Diaz <emmanueldiaz707@gmail.com>
2025-12-17 11:18:52 -0300

    agregar consignas final 2

---
+++ parciales_viejos/final2.pl
@@ -0,0 +1,12 @@
+/*
+   Escriba la función Coinciden, que tome como entrada una lista L de números (sin sublistas)
+   y una lista M que contiene sublistas, y devuelva otra lista formada únicamente por sublistas de un solo nivel.
+   Estas sublistas deben ser aquellas de M que incluyan todos los elementos de L en su nivel 1
+   (es decir, sin considerar elementos que estén dentro de sub- sublistas).
+
+   L = [2, 6]
+   M = [[6, 2, [1, 2], 3], [8, 6, 4], [2, [2, 2], 7, 5, 6], [9, 1, 6]]
+
+   R = [[6, 2, 3], [2, 7, 5, 6]]
+
+*/
