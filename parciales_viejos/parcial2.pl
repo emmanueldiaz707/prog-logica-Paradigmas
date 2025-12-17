a310855e856a74f89fa6d2eb98113b8a84c89d77
Emmanuel Diaz <emmanueldiaz707@gmail.com>
2025-12-17 08:18:58 -0300

    agregar consignas parcial 2

---
+++ parciales_viejos/parcial2.pl
@@ -0,0 +1,26 @@
+/*
+   Escriba la función que tome como entrada una lista con sublistas de un solo nivel y una posición P,
+   y elimine de cada sublista los elementos que están en la posición P.
+
+   L = [[3, 8, 4, 2, 3], [1, 6, 3, 9, 8], [12, 15]]
+   P = 3       ^                ^
+
+   R = [[3, 8, 2, 3], [1, 6, 9, 8], [12, 15]]
+*/
+  
+  
+  
+/*
+   Escriba la función que tome como entrada una lista de pares ordenados y una lista de números (sin sublistas),
+   y devuelva otra lista que contenga una sublista por cada par, donde cada sublista incluya los elementos de
+   la lista de números que pertenecen a la secuencia aritmética definida por dicho par.
+   Cada par (A, S) define una secuencia que comienza en A y aumenta en S unidades cada paso (A, A+S, A+2S, ...).
+   Si S es negativo, la sublista correspondiente debe estar vacía.
+   Cada número puede aparecer en múltiples sublistas o en ninguna.
+   Los elementos en cada sublista deben mantener el orden de aparición en la lista original.
+
+   Lista de pares   = [[3, 2], [6, 0], [12, 3], [7, 1]]
+   Lista de números = [4, 6, 4, 10, 3, 2, 5]
+   Resultado        = [[3, 5], [6], [], [10]]
+*/
+
