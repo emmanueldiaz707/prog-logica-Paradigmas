d2f70e05a00b753342d961f33fb4e1703660cb81
Emmanuel Diaz <emmanueldiaz707@gmail.com>
2025-12-17 10:29:07 -0300

    agregar consignas final 1

---
+++ parciales_viejos/final1.pl
@@ -0,0 +1,8 @@
+/*
+   Escriba una función que tome como entrada una lista con sublistas L y reordene los elementos de la lista principal y cada sublista
+   de tal manera que queden todos los números al principio y las listas al final (de la lista y cada sublista).
+
+   L = [2, 8, [3, 1], 2, [7, 3, 10, [4, 2], 9, [1]], 4]
+
+   R = [2, 8, 2, 4, [3, 1], [7, 3, 10, 9, [4, 2], [1]]]
+*/
